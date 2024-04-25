fa710ffad6fab9ac4312a33c7c342b27c790747c
hamad <nhamad919@gmail.com>
2024-04-26 00:24:00 +0400

    Added an animated splash screen for the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     testImplementation (libs.junit)
     androidTestImplementation (libs.androidx.junit)
     androidTestImplementation (libs.androidx.espresso.core)
-    implementation (libs.androidx.recyclerview)
+    implementation(libs.androidx.core.splashscreen)
 }
--- app/src/main/java/com/example/wallpapermixer/MainActivity.kt
+++ app/src/main/java/com/example/wallpapermixer/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.wallpapermixer
 
+import android.animation.ObjectAnimator
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import android.widget.*
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     private lateinit var imageView1: ImageView
     private lateinit var imageView2: ImageView
     private lateinit var clearButton: Button
@@ -15,6 +22,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
+        }
         setContentView(R.layout.activity_main)
 
         imageView1 = findViewById(R.id.imageView)
